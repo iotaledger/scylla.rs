fba434f5bdc48cc520dbd8a5e9bea53ed0bd715b
Louay Kamel <lk@overdb.com>
2021-02-04 19:46:15 -0500

    Add license template

--- scylla-cql/src/lib.rs
+++ scylla-cql/src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#![warn(missing_docs)]
+pub mod compression;
+pub mod connection;
+pub mod frame;
+pub mod murmur3;
+
+/// This is the public API of this crate
+pub use frame::*;
+
+// TODO add connection
