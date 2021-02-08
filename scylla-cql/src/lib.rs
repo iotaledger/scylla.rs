f6686450518daf67ab11f30d8a98ed397ec05955
Louay Kamel <lk@overdb.com>
2021-02-08 18:24:59 -0500

    Impl Into<Tcpstream> for cql

--- scylla-cql/src/lib.rs
+++ scylla-cql/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
-//! This crate implements decoder/encoder for a Cassandra frame and the associated protocol.
+
+//! This crate implements the binary Cql protocol V4.
 //! See `https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v4.spec` for more details.
 
 #![warn(missing_docs)]
