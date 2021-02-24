00c038c6f6bdf314fffede932392c7377ccd6f37
Alex Coats <alexandcoats@gmail.com>
2021-02-24 09:49:50 -0500

    Revert "Enhance id method"
    This reverts commit 79fb46c0a90a4d01b3ab497b8b10ddb83cd6c9b4.

--- scylla-cql/src/frame/execute.rs
+++ scylla-cql/src/frame/execute.rs
@@ -61,9 +61,9 @@ impl ExecuteBuilder<ExecuteHeader> {
 
 impl ExecuteBuilder<ExecuteId> {
     /// Set the id in the execute frame.
-    pub fn id(mut self, id: [u8; 16]) -> ExecuteBuilder<ExecuteConsistency> {
+    pub fn id(mut self, id: &str) -> ExecuteBuilder<ExecuteConsistency> {
         self.buffer.extend(&u16::to_be_bytes(id.len() as u16));
-        self.buffer.extend(&id);
+        self.buffer.extend(id.bytes());
         ExecuteBuilder::<ExecuteConsistency> {
             buffer: self.buffer,
             stage: ExecuteConsistency,
@@ -485,7 +485,7 @@ mod tests {
     // note: junk data
     fn simple_query_builder_test() {
         let Execute(_payload) = Execute::new()
-            .id([0; 16])
+            .id("HASHED_MD5_STATEMENT")
             .consistency(Consistency::One)
             .value("HASH_VALUE")
             .value("PAYLOAD_VALUE")
