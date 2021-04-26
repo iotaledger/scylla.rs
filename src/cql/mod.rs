b35c5e26f5cefa636b7a37e4ccacd6d93b6b82d0
Alex Coats <alexandcoats@gmail.com>
2021-04-26 09:41:38 -0400

    Small rafactoring

--- src/cql/mod.rs
+++ src/cql/mod.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! This crate implements the binary Cql protocol V4.
+//! This module implements the binary Cql protocol V4.
 //! See `https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v4.spec` for more details.
 
 #![warn(missing_docs)]
@@ -12,7 +12,7 @@ mod murmur3;
 mod tests;
 
 pub use connection::*;
-/// This is the public API of this crate
+/// This is the public API of this module
 pub use frame::*;
 
 pub use murmur3::murmur3_cassandra_x64_128;
--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,15 @@ pub use cql::*;
 #[cfg(feature = "app")]
 pub mod app;
 
+#[cfg(feature = "app")]
 pub mod prelude {
-    #[cfg(feature = "app")]
-    pub use super::app::{access::*, worker::*, *};
-    #[cfg(feature = "app")]
-    pub use backstage::*;
-
-    pub use super::cql::{
-        Batch, ColumnDecoder, ColumnEncoder, ColumnValue, Consistency, Decoder, Frame, Iter, Prepare,
-        PreparedStatement, Query, QueryStatement, Row, Rows, RowsDecoder, Statements, TokenEncoder, Values,
-        VoidDecoder,
+    pub use super::{
+        app::{access::*, worker::*, *},
+        cql::{
+            Batch, ColumnDecoder, ColumnEncoder, ColumnValue, Consistency, Decoder, Frame, Iter, Prepare,
+            PreparedStatement, Query, QueryStatement, Row, Rows, RowsDecoder, Statements, TokenEncoder, Values,
+            VoidDecoder,
+        },
     };
+    pub use backstage::*;
 }
