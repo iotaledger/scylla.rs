7178f01f747acce9bbb6c460f15f65c56fd1f237
Alexandcoats <alexandcoats@gmail.com>
2022-03-10 17:57:09 -0500

    Refactor receiver

--- scylla-rs/src/app/stage/receiver/mod.rs
+++ scylla-rs/src/app/stage/receiver/mod.rs
@@ -24,19 +24,17 @@ use tokio::{
     net::tcp::OwnedReadHalf,
 };
 
-const CQL_FRAME_HEADER_BYTES_LENGTH: usize = 9;
+const FRAME_HEADER_LEN: usize = 9;
 
 /// Receiver state
 pub(super) struct Receiver<C: Compression> {
     appends_num: u16,
-    buffer_size: usize,
     _compression: PhantomData<fn(C) -> C>,
 }
 
 impl<C: Compression> Receiver<C> {
-    pub(super) fn new(buffer_size: Option<usize>, appends_num: u16) -> Self {
+    pub(super) fn new(appends_num: u16) -> Self {
         Self {
-            buffer_size: buffer_size.unwrap_or(1024000),
             appends_num,
             _compression: PhantomData,
         }
@@ -63,56 +61,44 @@ where
         Ok((payloads, reporters_handles))
     }
     async fn run(&mut self, rt: &mut Rt<Self, S>, (payloads, reporters_handles): Self::Data) -> ActorResult<()> {
-        let mut idx = 0;
-        let mut header = None;
-        let mut buffer = vec![0; self.buffer_size];
-        while let Ok(n) = rt.inbox_mut().read(&mut buffer[idx..]).await {
+        let mut header_buf = [0; FRAME_HEADER_LEN];
+        while let Ok(n) = rt.inbox_mut().read_exact(&mut header_buf).await {
             if n == 0 {
                 break;
             }
-            idx += n;
-            while idx > CQL_FRAME_HEADER_BYTES_LENGTH {
-                if header.is_none() {
-                    header.replace(Header::try_from(&buffer[..CQL_FRAME_HEADER_BYTES_LENGTH]).map_err(|e| {
-                        log::error!("{}", e);
-                        ActorError::exit(e)
-                    })?);
+            let header = Header::try_from(header_buf).map_err(|e| {
+                log::error!("{}", e);
+                ActorError::exit(e)
+            })?;
+            let mut body_buf = vec![0; header.body_len() as usize];
+            if let Ok(n) = rt.inbox_mut().read_exact(&mut body_buf).await {
+                if n == 0 {
+                    break;
                 }
-                if let Some(h) = header {
-                    let frame_len = h.body_len() as usize + CQL_FRAME_HEADER_BYTES_LENGTH;
-                    if idx >= frame_len {
-                        // get mut ref to payload for stream_id
-                        let mut payload = payloads[h.stream() as usize]
-                            .as_mut()
-                            .take()
-                            .ok_or_else(|| anyhow!("No payload for stream {}!", h.stream()))?;
-                        payload.resize(frame_len, 0);
-                        payload.copy_from_slice(&buffer[..frame_len]);
-                        // tell reporter that the payload is ready.
-                        let reporter_handle = reporters_handles
-                            .get(&compute_reporter_num(h.stream(), self.appends_num))
-                            .ok_or_else(|| anyhow!("No reporter handle for stream {}!", h.stream()))?;
+                let frame_len = header.body_len() as usize + FRAME_HEADER_LEN;
+                // get mut ref to payload for stream_id
+                let mut payload = payloads[header.stream() as usize]
+                    .as_mut()
+                    .take()
+                    .ok_or_else(|| anyhow!("No payload for stream {}!", header.stream()))?;
+                payload.resize(frame_len, 0);
+                payload[..FRAME_HEADER_LEN].copy_from_slice(&header_buf);
+                payload[FRAME_HEADER_LEN..].copy_from_slice(&body_buf);
+                // tell reporter that the payload is ready.
+                let reporter_handle = reporters_handles
+                    .get(&compute_reporter_num(header.stream(), self.appends_num))
+                    .ok_or_else(|| anyhow!("No reporter handle for stream {}!", header.stream()))?;
 
-                        match ResponseFrame::decode::<C>(payload) {
-                            Ok(frame) => {
-                                reporter_handle
-                                    .send(ReporterEvent::Response { frame })
-                                    .unwrap_or_else(|e| log::error!("{}", e));
-                            }
-                            Err(e) => {
-                                reporter_handle
-                                    .send(ReporterEvent::Err(anyhow::anyhow!(e), h.stream()))
-                                    .unwrap_or_else(|e| log::error!("{}", e));
-                            }
-                        }
-
-                        header = None;
-                        if idx > frame_len {
-                            buffer.copy_within(frame_len..idx, 0)
-                        }
-                        idx -= frame_len;
-                    } else {
-                        break;
+                match ResponseFrame::decode::<C>(payload) {
+                    Ok(frame) => {
+                        reporter_handle
+                            .send(ReporterEvent::Response { frame })
+                            .unwrap_or_else(|e| log::error!("{}", e));
+                    }
+                    Err(e) => {
+                        reporter_handle
+                            .send(ReporterEvent::Err(anyhow::anyhow!(e), header.stream()))
+                            .unwrap_or_else(|e| log::error!("{}", e));
                     }
                 }
             }
